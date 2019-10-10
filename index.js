012b2a6916e76ee767448135299fdd7a2feb8457
Dwika Putra <dwikaputra15@gmail.com>
2019-10-10 09:11:06 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const Sequelize = require("sequelize");
+const { WebhookClient, Card } = require("dialogflow-fulfillment");
+const app = express();
+const port = process.env.PORT;
+const sequelize = new Sequelize("db_library", "dwikaputra15", "Matahari54321", {
+  host: "db4free.net",
+  dialect: "mysql"
+});
+app.use(express.json());
+
+app.post("/", (request, response, next) => {
+  const agent = new WebhookClient({ request, response });
+  let intent = new Map();
+  agent.handleRequest(intent);
+});
+
+app.listen(port, () => {
+  console.log(`server start at ${port}`);
+});
