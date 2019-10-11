4b5668cbda1ae969320bfc320d9cdbd2f44c133b
Dwika Putra <dwikaputra15@gmail.com>
2019-10-11 09:05:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,41 +16,43 @@ app.post("/", (request, response, next) => {
 
   const awalan = async agent => {
     try {
-      const {
-        message,
-        sender
-      } = request.body.originalDetectIntentRequest.payload.data;
+      console.log(JSON.stringify(request.body));
+      agent.add("Hola");
+      // const {
+      //   message,
+      //   sender
+      // } = request.body.originalDetectIntentRequest.payload.data;
 
-      const [user] = await sequelize.query(
-        `SELECT * FROM tb_user WHERE tb_user.id_user = ${sender.id}`
-      );
-      const [result] = await sequelize.query(
-        "SELECT tb_respon.respon FROM tb_respon WHERE tb_respon.intent = `awal`"
-      );
+      // const [user] = await sequelize.query(
+      //   `SELECT * FROM tb_user WHERE tb_user.id_user = ${sender.id}`
+      // );
+      // const [result] = await sequelize.query(
+      //   "SELECT tb_respon.respon FROM tb_respon WHERE tb_respon.intent = `awal`"
+      // );
 
-      if (user.length > 0) {
-        let respon = result[0].respon.replace("$nama_user", user[0].nama_user);
-        respon = respon.replace("$pesan", message.text);
+      // if (user.length > 0) {
+      //   let respon = result[0].respon.replace("$nama_user", user[0].nama_user);
+      //   respon = respon.replace("$pesan", message.text);
 
-        agent.add(respon);
-        agent.add(
-          new Card({
-            title: "LibraryBot",
-            buttonText: "Booking Buku",
-            buttonUrl: "booking"
-          })
-        );
-      } else {
-        const respon = result[1].respon.replace("$pesan", message.text);
-        agent.add(respon);
-        agent.add(
-          new Card({
-            title: "LibraryBot",
-            buttonText: "Daftar Akun",
-            buttonUrl: "daftar"
-          })
-        );
-      }
+      //   agent.add(respon);
+      //   agent.add(
+      //     new Card({
+      //       title: "LibraryBot",
+      //       buttonText: "Booking Buku",
+      //       buttonUrl: "booking"
+      //     })
+      //   );
+      // } else {
+      //   const respon = result[1].respon.replace("$pesan", message.text);
+      //   agent.add(respon);
+      //   agent.add(
+      //     new Card({
+      //       title: "LibraryBot",
+      //       buttonText: "Daftar Akun",
+      //       buttonUrl: "daftar"
+      //     })
+      //   );
+      // }
     } catch (error) {
       console.log(error);
       agent.add("Mohon untuk mengulang menginputkan kembali");
